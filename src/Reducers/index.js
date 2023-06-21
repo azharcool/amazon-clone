0407462abd881f6e4c10a4f823ab3cae0679a2da
azharcool <azharuddinmullah219@gmail.com>
2023-06-21 17:25:18 +0530

    multi-language

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -3,12 +3,14 @@ import ProductsReducer from "./ProductsReducer";
 import CartItemsReducer from "./CartItemsReducer";
 import SetUserReducer from "./setUserReducer";
 import SignedInReducer from "./SignedInReducer";
+import languageReducer from "./languageReducer";
 
 const allReducers = combineReducers({
   products: ProductsReducer,
   cart: CartItemsReducer,
   user: SetUserReducer,
   signedIn: SignedInReducer,
+  language: languageReducer,
 });
 
 export default allReducers;
